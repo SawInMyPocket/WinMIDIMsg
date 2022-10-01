2c7006322bec0ec49a1178951cf82ee646a2c22a
Cole McK <c013mck@gmail.com>
2022-09-30 17:39:24 -0700

    Added CTest to CMakeLists Added input and output tests to the demo program, the results can be viewed if CTest is in verbose mode

--- wmmDemo.cpp
+++ wmmDemo.cpp
@@ -11,11 +11,42 @@ using std::wcout;
 using std::wcin;
 using std::endl;
 
-int main(/*int argc, char *argv[]*/) {
+/* 
+no options just lists the available MIDI devices
+'i #' for input device test
+'o #' for output device test
+'m' for a manual testing mode
+*/
+int main(int argc, char *argv[]) {
 	wcout << "Test program for the WinMIDIMsg module" << endl << endl;
 	
 	// poll MIDI devices
 	wcout << PrintMIDIDevices() << endl;
-	// open output device and send some messages?
-	// open input device and listen a little bit?
+
+	// pass arguments of the form '[i|o] [#]'
+	if (argc > 2) {
+		// open output device and send some messages
+		if (argv[1][0] == 'o') {
+			int outputDeviceNumber = argv[2][0] - '0';
+			if (outputDeviceNumber >= GetMIDIOutputDeviceCount()) {
+				wcout << outputDeviceNumber << " is not a valid output device index." << endl;
+				return 0;
+			}
+			OutputMidiDevice outMidi(outputDeviceNumber);
+			outMidi.debug = true;
+			wcout << outMidi.SelfIdentify() << endl;
+			outMidi.beep();
+		}
+		// open input device and listen a little bit	
+		if (argv[1][0] == 'i') {
+			int inputDeviceNumber = argv[2][0] - '0';
+			if (inputDeviceNumber >= GetMIDIInputDeviceCount()) {
+				wcout << inputDeviceNumber << " is not a valid input device index." << endl;
+				return 0;
+			}
+			InputMidiDevice inMidi(inputDeviceNumber);
+			inMidi.debug = true;
+			wcout << inMidi.SelfIdentify() << endl;
+		}
+	}
 }
